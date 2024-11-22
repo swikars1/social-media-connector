2b9d285f4ceb08959d8f4f725c433dd72ef6303b
swikars1 <swikarsharma@gmail.com>
2024-11-22 13:03:09 +0545

    build re

--- src/lib/signInLib.ts
+++ src/lib/signInLib.ts
@@ -4,6 +4,11 @@ import { signIn } from "@/auth";
 
 export async function signInToTiktok() {
   await signIn("tiktok", {
-    scopes: ["user.info.basic", "video.publish", "video.upload"],
+    scopes: [
+      "user.info.basic",
+      "user.info.profile",
+      "video.publish",
+      "video.upload",
+    ],
   });
 }
