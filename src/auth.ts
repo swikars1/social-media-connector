cbec647e3df0c74b9c5fb8e9ae5545f3892e5e35
swikars1 <swikarsharma@gmail.com>
2024-11-22 11:31:02 +0545

    build re

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     TikTok({
       authorization: {
         params: {
+          client_key: process.env.AUTH_TIKTOK_ID,
           scope: "user.info.profile video.publish video.upload",
         },
       },
