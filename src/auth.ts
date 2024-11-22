2c66245430f7793a1a5c7b311f150b953579df4a
swikars1 <swikarsharma@gmail.com>
2024-11-22 11:28:00 +0545

    scopes

--- src/auth.ts
+++ src/auth.ts
@@ -2,5 +2,13 @@ import NextAuth from "next-auth";
 import TikTok from "next-auth/providers/tiktok";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [TikTok],
+  providers: [
+    TikTok({
+      authorization: {
+        params: {
+          scope: "user.info.profile video.publish video.upload",
+        },
+      },
+    }),
+  ],
 });
